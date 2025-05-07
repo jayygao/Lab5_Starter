4768f0a6ac97da8b8bf9a17f60fe4db48288f6d4
jayygao <jug015@ucsd.edu>
2025-05-07 02:08:14 -0700

    testing branch: should be wrong

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,52 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('valid phone', () => {
+  expect(isPhoneNumber('666-222-4444')).toBe(true);
+  expect(isPhoneNumber('(666)222-4444')).toBe(true);
+});
+
+test('invalid phone', () => {
+  expect(isPhoneNumber('666222444')).toBe(false);
+  expect(isPhoneNumber('ski-332-toil')).toBe(false);
+});
+
+test('valid emails', () => {
+  expect(isEmail('joe@ucsd.com')).toBe(true);
+  expect(isEmail('joe@gmail.com')).toBe(true);
+});
+
+test('invalid emails', () => {
+  expect(isEmail('invalid')).toBe(false);
+  expect(isEmail('invalid.com')).toBe(false);
+});
+
+test('valid strong', () => {
+  expect(isStrongPassword('J0929')).toBe(true);
+  expect(isStrongPassword('bye_p12')).toBe(true);
+});
+
+test('invalid strong', () => {
+  expect(isStrongPassword('123a')).toBe(false); 
+  expect(isStrongPassword('joe!@')).toBe(false); 
+});
+
+test('valid dates', () => {
+  expect(isDate('5/7/2025')).toBe(true);
+  expect(isDate('09/29/2022')).toBe(true);
+});
+
+test('invalid dates', () => {
+  expect(isDate('20252/7/203')).toBe(false); 
+  expect(isDate('5-7-2025')).toBe(false); 
+});
+
+test('valid hex colors', () => {
+  expect(isHexColor('#312')).toBe(true);
+  expect(isHexColor('000000')).toBe(true);
+});
+
+test('invalid hex colors', () => {
+  expect(isHexColor('#9999')).toBe(false);
+  expect(isHexColor('joe982231')).toBe(false);
+});
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,57 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  let words = document.getElementById('text-to-speak');
+  let selected = document.getElementById('voice-select');
+  let button = document.querySelector('#explore button');
+  let face = document.querySelector('#explore img');
+
+  let persons = window.speechSynthesis;
+  let choices = [];
+
+  function getList() {
+    let unused = persons.getVoices();
+
+    if (!unused.length || choices.length) {
+      return;
+    }
+    let used = new Set();
+  
+    unused.forEach((voice, i) => {
+      const item = `${voice.name} (${voice.lang})`;
+      if (used.has(item)) {
+        return;
+      }
+      let choice = document.createElement('option');
+      choice.value = i;
+      choice.textContent = item;
+      selected.appendChild(choice);
+  
+      used.add(item);
+    });
+  
+    choices = unused;
+  }
+
+  persons.onvoiceschanged = getList;
+  getList();
+
+  button.addEventListener('click', speak);
+  function speak() {
+    let userInput = words.value;
+    let select = Number(selected.value);
+    let voiceOutput = new SpeechSynthesisUtterance();
+    voiceOutput.text = userInput;
+    voiceOutput.voice = choices[select];
+
+    voiceOutput.onstart = () => {
+      face.src = 'assets/images/smiling-open.png';
+    };
+
+    voiceOutput.onend = () => {
+      face.src = 'assets/images/smiling.png';
+    };
+
+    persons.speak(voiceOutput);
+  }
 }
